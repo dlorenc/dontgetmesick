ce550052a4c59b11e8081ddfbac8a1d75ff23f4d
James Counterman <james.m.counterman@gmail.com>
2013-03-28 20:56:21 -0300

    added *_ds to deferentiate

--- datastore.py
+++ datastore.py
@@ -2,7 +2,7 @@ from google.appengine.ext import db
 
 
 class Sick(db.Model):
-    sick_person_email = db.StringProperty(required=True)
-    sick_person_name = db.StringProperty(required=True)
-    boss_email = db.StringProperty(required=True)
-    date = db.DateTimeProperty(auto_now_add=True)
+    sick_person_email_ds = db.StringProperty(required=True)
+    sick_person_name_ds = db.StringProperty(required=True)
+    boss_email_ds = db.StringProperty(required=True)
+    date_ds = db.DateTimeProperty(auto_now_add=True)
